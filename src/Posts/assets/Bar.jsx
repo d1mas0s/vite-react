5ece5b18239c9c82de1eab398c8016107a4e0471
Дмитрий Седов <d-sedov@asoft21.ru>
2024-07-08 15:18:29 +0300

    Feat: Highlight buttons in Bar component according to shown group

--- src/Posts/assets/Bar.jsx
+++ src/Posts/assets/Bar.jsx
@@ -5,12 +5,12 @@ const Bar = ({page, setPage, lastPage}) => {
       <div>
         <button className="PagiButton" onClick={page>1?()=>setPage(page-1):()=>setPage(1)}>prev</button>
         
-        <button className="PagiButton" onClick={()=>setPage(1)}>1</button>
-        <button className="PagiButton" onClick={()=>setPage(2)}>2</button>
+        <button className={page==1?"PagiButtonActive":"PagiButton"} onClick={()=>setPage(1)}>1</button>
+        <button className={page==2?"PagiButtonActive":"PagiButton"} onClick={()=>setPage(2)}>2</button>
         {/* <input onChange={(e)=>setPage(e.target.value)}></input> */} 
-        <button className="PagiButton" >...</button>
-        <button className="PagiButton" onClick={()=>setPage(l-1)}>{l-1}</button>
-        <button className="PagiButton" onClick={()=>setPage(l)}>{l}</button>
+        <button className={(page>2&&page<l-1)?"PagiButtonActive":"PagiButton"} >...</button>
+        <button className={page==l-1?"PagiButtonActive":"PagiButton"} onClick={()=>setPage(l-1)}>{l-1}</button>
+        <button className={page==l?"PagiButtonActive":"PagiButton"} onClick={()=>setPage(l)}>{l}</button>
 
         <button className="PagiButton" onClick={page<l?()=>setPage(page+1):()=>setPage(l)}>next</button>
       </div>
