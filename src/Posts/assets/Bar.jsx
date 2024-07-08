f98e8a6a2f7c52aebff385094ce204c985ade24f
Дмитрий Седов <d-sedov@asoft21.ru>
2024-07-08 14:20:51 +0300

    Refactor: Change access to properties in Bar component

--- src/Posts/assets/Bar.jsx
+++ src/Posts/assets/Bar.jsx
@@ -1,17 +1,17 @@
 
-const Bar = (props) => {
-    const l = props.lastPage;
+const Bar = ({page, setPage, lastPage}) => {
+    const l = lastPage;
     return (
       <div>
-        <button className="PagiButton" onClick={props.page>1?()=>props.setPage(props.page-1):()=>props.setPage(1)}>prev</button>
+        <button className="PagiButton" onClick={page>1?()=>setPage(page-1):()=>setPage(1)}>prev</button>
         
-        <button className="PagiButton" onClick={()=>props.setPage(1)}>1</button>
-        <button className="PagiButton" onClick={()=>props.setPage(2)}>2</button>
-        {/* <input onChange={(e)=>props.setPage(e.target.value)}></input> */} 
-        <button className="PagiButton" onClick={()=>props.setPage(l-1)}>{l-1}</button>
-        <button className="PagiButton" onClick={()=>props.setPage(l)}>{l}</button>
+        <button className="PagiButton" onClick={()=>setPage(1)}>1</button>
+        <button className="PagiButton" onClick={()=>setPage(2)}>2</button>
+        {/* <input onChange={(e)=>setPage(e.target.value)}></input> */} 
+        <button className="PagiButton" onClick={()=>setPage(l-1)}>{l-1}</button>
+        <button className="PagiButton" onClick={()=>setPage(l)}>{l}</button>
 
-        <button className="PagiButton" onClick={props.page<l?()=>props.setPage(props.page+1):()=>props.setPage(l)}>next</button>
+        <button className="PagiButton" onClick={page<l?()=>setPage(page+1):()=>setPage(l)}>next</button>
       </div>
     );
   };
