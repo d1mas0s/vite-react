a7ca70e9737c5024a230c7175b9e525a516c9e43
Дмитрий Седов <d-sedov@asoft21.ru>
2024-07-09 14:35:24 +0300

    feat: Highlight buttons in Bar component on hover

--- src/Posts/assets/Bar.jsx
+++ src/Posts/assets/Bar.jsx
@@ -1,28 +1,18 @@
 
-const Bar = ({page, setPage, lastPage, setPostsPerPage}) => {
+const Bar = ({page, setPage, lastPage}) => {
     const l = lastPage;
     return (
       <div>
-        <div>
-          <button className="PagiButton" onClick={setPostsPerPage(1)}>1</button>
-          <button className="PagiButton" onClick={setPostsPerPage(5)}>5</button>
-          <button className="PagiButton" onClick={setPostsPerPage(10)}>10</button>
-          <button className="PagiButton" onClick={setPostsPerPage(20)}>20</button>
-          <button className="PagiButton" onClick={setPostsPerPage(50)}>50</button>
-        </div>
+        <button className="PagiButton" onClick={page>1?()=>setPage(page-1):()=>setPage(1)}>prev</button>
+        
+        <button className={page==1?"PagiButtonActive":"PagiButton"} onClick={()=>setPage(1)}>1</button>
+        <button className={page==2?"PagiButtonActive":"PagiButton"} onClick={()=>setPage(2)}>2</button>
+        {/* <input onChange={(e)=>setPage(e.target.value)}></input> */} 
+        <button className={(page>2&&page<l-1)?"PagiButtonActive":"PagiButton"} >...</button>
+        <button className={page==l-1?"PagiButtonActive":"PagiButton"} onClick={()=>setPage(l-1)}>{l-1}</button>
+        <button className={page==l?"PagiButtonActive":"PagiButton"} onClick={()=>setPage(l)}>{l}</button>
 
-        <div>
-          <button className="PagiButton" onClick={page>1?()=>setPage(page-1):()=>setPage(1)}>prev</button>
-          
-          <button className={page==1?"PagiButtonActive":"PagiButton"} onClick={()=>setPage(1)}>1</button>
-          <button className={page==2?"PagiButtonActive":"PagiButton"} onClick={()=>setPage(2)}>2</button>
-          {/* <input onChange={(e)=>setPage(e.target.value)}></input> */} 
-          <button className={(page>2&&page<l-1)?"PagiButtonActive":"PagiButton"} >...</button>
-          <button className={page==l-1?"PagiButtonActive":"PagiButton"} onClick={()=>setPage(l-1)}>{l-1}</button>
-          <button className={page==l?"PagiButtonActive":"PagiButton"} onClick={()=>setPage(l)}>{l}</button>
-
-          <button className="PagiButton" onClick={page<l?()=>setPage(page+1):()=>setPage(l)}>next</button>
-        </div>
+        <button className="PagiButton" onClick={page<l?()=>setPage(page+1):()=>setPage(l)}>next</button>
       </div>
     );
   };
--- src/Posts/assets/List.jsx
+++ src/Posts/assets/List.jsx
@@ -7,6 +7,8 @@ const Posts = () => {
     const [postsPerPage, setPostsPerPage] = useState(5);
     const [lastPage, setLastPage] = useState(20);
 
+    //setLastPage(Math.ceil(posts.length/postsPerPage))
+
     useEffect(() => {
       async function fetchData() {
         try {
@@ -19,12 +21,6 @@ const Posts = () => {
       }
       fetchData(); 
     }, []); 
-
-    useEffect(()=>{
-        setLastPage(Math.ceil(posts.length/postsPerPage));
-        console.log(postsPerPage);
-    }, [posts.length, postsPerPage]);
-
     return (
       <div>
         <Bar page={page} setPage={setPage} lastPage={lastPage} setPostsPerPage={setPostsPerPage}/>
