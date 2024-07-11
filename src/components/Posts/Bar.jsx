d2f4e62c0c59d58a02f895fbafe2275cc0d0a438
Дмитрий Седов <d-sedov@asoft21.ru>
2024-07-11 16:25:02 +0300

    feat(navigation): Show current group indexes on navigation buttons.

--- src/components/Posts/Bar.jsx
+++ src/components/Posts/Bar.jsx
@@ -1,15 +1,19 @@
 
 const Bar = ({posts, page, setPage, lastPage}) => {
     const l = lastPage;
-    console.log(posts);
     if (posts.length != 0) return (
       <>
         <button className="PagiButton" onClick={page>1?()=>setPage(page-1):()=>setPage(1)}>prev</button>
         
-        <button className={page==1?"PagiButtonActive":"PagiButton"} onClick={()=>setPage(1)}>1</button>
-        <button className={page==2?"PagiButtonActive":"PagiButton"} onClick={()=>setPage(2)}>2</button>
-        {/* <input onChange={(e)=>setPage(e.target.value)}></input> */} 
-        <button className={(page>2&&page<l-1)?"PagiButtonActive":"PagiButton"} >...</button>
+        <button className={page%2==1&&page<l-1?"PagiButtonActive":"PagiButton"}
+        onClick={()=>setPage(page!=l&&page!=l-1?(page%2==1?page:page-1):l-3)}>
+        {page!=l&&page!=l-1?(page%2==1?page:page-1):l-3}</button>
+       
+        <button className={page%2==0&&page<l-1?"PagiButtonActive":"PagiButton"}
+        onClick={()=>setPage(page!=l&&page!=l-1?(page%2==0?page:page+1):l-2)}>
+        {page!=l&&page!=l-1?(page%2==0?page:page+1):l-2}</button>
+        
+        <button className="PagiButton" >...</button>
         <button className={page==l-1?"PagiButtonActive":"PagiButton"} onClick={()=>setPage(l-1)}>{l-1}</button>
         <button className={page==l?"PagiButtonActive":"PagiButton"} onClick={()=>setPage(l)}>{l}</button>
 
